3422de8fdd26e84780c739ebfaa9cad11ef528c0
Luca Carboni <l.carboni@qaamgo.com>
2017-01-11 16:26:33 +0100

    Added statistics endpoint handler

--- src/Endpoint/StatisticsEndpoint.php
+++ src/Endpoint/StatisticsEndpoint.php
@@ -16,16 +16,17 @@ class StatisticsEndpoint extends Abstracted
     /**
      * Returns statistics info for a specific day passed in the format yyyy-mm-dd
      *
-     * @param string $day
+     * @param string $day    The day to search statistics for in the format yyyy-mm-dd
+     * @param string $filter Can be 'single'|'all'. In this case, statistics for all of the user's API keys are returned
      *
      * @return array
      *
      * @throws OnlineConvertSdkException when error on the requests
      */
-    public function getStatsByDay($day)
+    public function getStatsByDay($day, $filter = 'single')
     {
         try {
-            $url = $this->client->generateUrl(Resources::STATS_DAY, ['day' => $day]);
+            $url = $this->client->generateUrl(Resources::STATS_DAY . '/' . $filter, ['day' => $day]);
 
             return $this->responseToArray(
                 $this->client->sendRequest($url, OnlineConvertClient::METHOD_GET)
@@ -38,14 +39,15 @@ class StatisticsEndpoint extends Abstracted
     /**
      * Returns statistics info for a specific month passed in the format yyyy-mm
      *
-     * @param string $month
+     * @param string $month  The month to search statistics for in the format yyyy-mm
+     * @param string $filter Can be 'single'|'all'. In this case, statistics for all of the user's API keys are returned
      *
      * @return array
      */
-    public function getStatsByMonth($month)
+    public function getStatsByMonth($month, $filter = 'single')
     {
         try {
-            $url = $this->client->generateUrl(Resources::STATS_MONTH, ['month' => $month]);
+            $url = $this->client->generateUrl(Resources::STATS_MONTH . '/' . $filter, ['month' => $month]);
 
             return $this->responseToArray(
                 $this->client->sendRequest($url, OnlineConvertClient::METHOD_GET)
@@ -58,14 +60,15 @@ class StatisticsEndpoint extends Abstracted
     /**
      * Returns statistics info for a specific year passed in the format yyyy
      *
-     * @param string $year
+     * @param string $year   The year to search statistics for in the format yyyy
+     * @param string $filter Can be 'single'|'all'. In this case, statistics for all of the user's API keys are returned
      *
      * @return array
      */
-    public function getStatsByYear($year)
+    public function getStatsByYear($year, $filter = 'single')
     {
         try {
-            $url = $this->client->generateUrl(Resources::STATS_YEAR, ['year' => $year]);
+            $url = $this->client->generateUrl(Resources::STATS_YEAR . '/' . $filter, ['year' => $year]);
 
             return $this->responseToArray(
                 $this->client->sendRequest($url, OnlineConvertClient::METHOD_GET)
