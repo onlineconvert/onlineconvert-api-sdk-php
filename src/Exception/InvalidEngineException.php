c71e27d65f4a1190215ea30e7ad38b8fe337a969
online-convert <time2help@online-convert.com>
2020-06-02 11:32:22 +0200

    Specify donwloader for remote inputs
    * add possibility to add the downloader type for remote inputs to get the files in the needed way

---
+++ src/Exception/InvalidEngineException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OnlineConvert\Exception;
+
+/**
+ * Throwable when the chosen engine is invalid
+ *
+ * @package OnlineConvert\Exception
+ */
+class InvalidEngineException extends OnlineConvertSdkException
+{
+}
